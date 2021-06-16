16fb8d91e02b05479c8dfc27e83225f45c2134ef
hillel <zontovv@ouylook.com>
2021-06-16 20:28:11 +0300

    test

--- lesson3.py
+++ lesson3.py
@@ -1,3 +1,30 @@
-print("Hello, World")
+# if
+# if условие:
+#     блок если ДА
+# else:
+#     блок если НЕТ
 
-print(123)
+# my_bool = False
+# value = str(my_bool)
+# if value:
+#     print(value)
+# print("The end")
+
+# if value == 4:
+#     print("!!!!")
+#     print(value * 3)
+#     new_value = "asd" * value
+#     print(new_value)
+# else:
+#     print("Else case")
+#
+# print("The end")
+
+value = 15
+
+if not value % 2 and not value % 3:
+    print("Число делится и на 2 и на 3 - т.е. на 6")
+elif not value % 2:
+    print("Число делится только на 2")
+elif not value % 3:
+    print("Число делится только на 3")
