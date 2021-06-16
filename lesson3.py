2323e914257116e414c5d4b680d6c107bd57d45c
hillel <zontovv@ouylook.com>
2021-06-16 21:22:22 +0300

    lesson3

--- lesson3.py
+++ lesson3.py
@@ -4,21 +4,22 @@
 # else:
 #     блок если НЕТ
 
-# my_bool = False
-# value = str(my_bool)
-# if value:
-#     print(value)
-# print("The end")
+my_bool = False
+value = str(my_bool)
+if value:
+    print(value)
+print("The end")
 
-# if value == 4:
-#     print("!!!!")
-#     print(value * 3)
-#     new_value = "asd" * value
-#     print(new_value)
-# else:
-#     print("Else case")
-#
-# print("The end")
+value = 15
+if value == 4:
+    print("!!!!")
+    print(value * 3)
+    new_value = "asd" * value
+    print(new_value)
+else:
+    print("Else case")
+
+print("The end")
 
 value = 15
 
@@ -28,3 +29,16 @@ elif not value % 2:
     print("Число делится только на 2")
 elif not value % 3:
     print("Число делится только на 3")
+
+my_str = "qwety"
+my_int = 9
+if 'r' in my_str:
+    new_str = my_str * 2
+else:
+    new_str = my_str
+
+new_str = my_str * 2 if 'r' in my_str else my_str
+
+new_int = my_int // 2 if my_int > 10 else -1
+print(new_int)
+
